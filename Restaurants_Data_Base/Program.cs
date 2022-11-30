7f7b7c2bd606475cd585be4574edb23599d36ba9
RenoStomp <emilabdullaev@yahoo.com>
2022-11-30 19:37:04 +0600

    Cleaned

--- Restaurants_Data_Base/Program.cs
+++ Restaurants_Data_Base/Program.cs
@@ -4,24 +4,8 @@ using Restaurants_Data_Base.Menu;
 using Restaurants_Data_Base.Place;
 
 List<Ingredient> ingredients = FileReader.ReadIngredients();
-//Console.WriteLine();
-
 List<Meal> meals = FileReader.ReadMeals(ingredients);
-//foreach (var meal in meals)
-//{
-//    meal.ShowIngredientsAndPrice();
-//    Console.ReadKey();
-//}
-//Console.WriteLine();
-
 List<Restaurant> restaurants = FileReader.ReadRestaurants(meals);
-//foreach(Restaurant restaurant in restaurants)
-//{
-//    Console.WriteLine($"{restaurant.Name}"); 
-//}
-
-//Console.ReadKey();
-
 
 
 Menu.ExecuteMenu(restaurants, ingredients);
